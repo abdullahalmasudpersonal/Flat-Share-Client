ff91835e965c6f23a122c1f93d36bce4fd6c0274
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-05-26 01:58:01 +0600

    create flat post

--- src/app/(withDashboardLayout)/dashboard/seller/post-ad/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/seller/post-ad/page.tsx
@@ -4,10 +4,10 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button, Container, Typography } from "@mui/material";
 import { FieldValues } from "react-hook-form";
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 import Link from "next/link";
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import Form from "@/components/Forms/Form";
 import Input from "@/components/Forms/Input";
 import AutoFileUploader from "@/components/Forms/AutoFileUploader";
@@ -20,8 +20,8 @@ import { toast } from "sonner";
 const PostAd = () => {
   const { userId } = getUserInfo();
   ///const { data, isLoading } = useGetMYProfileQuery(undefined);
-  const [postFlat, { isLoading: updating }] =
-    usePostFlatMutation();
+  const [postFlat, { isLoading: updating }] = usePostFlatMutation();
+  const [rest, setReset] = React.useState();
 
   const fileUploadHandler = (file: File) => {
     const formData = new FormData();
@@ -31,26 +31,26 @@ const PostAd = () => {
   };
 
   const handleSubmit = async (values: FieldValues) => {
-    values.userId = userId
-    values.squareFeet = Number(values.squareFeet)
-    values.totalBedrooms = Number(values.totalBedrooms)
-    values.totalRooms = Number(values.totalRooms)
-    values.rent = Number(values.rent)
-    values.advanceAmount = Number(values.advanceAmount)
+    values.userId = userId;
+    values.squareFeet = Number(values.squareFeet);
+    values.totalBedrooms = Number(values.totalBedrooms);
+    values.totalRooms = Number(values.totalRooms);
+    values.rent = Number(values.rent);
+    values.advanceAmount = Number(values.advanceAmount);
     console.log(values);
 
     const data = modifyPayload(values);
-    console.log('modify', data);
+    console.log("modify", data);
 
     try {
       const res = await postFlat(data).unwrap();
       if (res?.id) {
-        toast.success("Doctor created successfully!!!");
+        toast.success("Flat created successfully!!!");
       }
     } catch (err: any) {
       console.error(err);
     }
-  }
+  };
   return (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
       <Box
@@ -67,69 +67,88 @@ const PostAd = () => {
           Post your Flat or Rooms
         </Typography>
 
-        <Form onSubmit={handleSubmit}>
-          <Input required
-
+        <Form onSubmit={handleSubmit} defaultValues={{}}>
+          <Input
+            required
             name="flatName"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="name"
+            label="Flat Name"
             size="small"
           />
-          <Input required name="squareFeet"
+          <Input
+            required
+            name="squareFeet"
             sx={{ width: "100%", marginBottom: "15px" }}
             label="squareFeet"
             type="number"
             size="small"
           />
-          <Input required name="totalBedrooms"
+          <Input
+            required
+            name="totalBedrooms"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="totalBedrooms"
+            label="Total Bedrooms"
             type="number"
             size="small"
           />
-          <Input required name="totalRooms"
+          <Input
+            required
+            name="totalRooms"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="totalRooms"
+            label="Total Rooms"
             type="number"
             size="small"
           />
-          <Input required name="utilitiesDescription"
+          <Input
+            required
+            name="utilitiesDescription"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="utilitiesDescription"
+            label="Utilities Description"
             size="small"
           />
-          <Input required name="location"
+          <Input
+            required
+            name="location"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="location"
+            label="Location"
             size="small"
           />
-          <Input required name="description"
+          <Input
+            required
+            name="description"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="description"
+            label="Description"
             size="small"
           />
-          <Input required name="amenities"
+          <Input
+            required
+            name="amenities"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="amenities"
+            label="Amenities"
             size="small"
           />
-          <Input required name="rent"
+          <Input
+            required
+            name="rent"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="rent"
+            label="Rent"
             type="number"
             size="small"
           />
-          <Input required name="advanceAmount"
+          <Input
+            required
+            name="advanceAmount"
             sx={{ width: "100%", marginBottom: "15px" }}
-            label="advanceAmount"
+            label="AdvanceAmount"
             type="number"
             size="small"
           />
-          <Box my={3}  >
+          <Box my={3}>
             {updating ? (
               <p>Uploading...</p>
             ) : (
-              <AutoFileUploader sx={{ width: "100%", }}
+              <AutoFileUploader
+                sx={{ width: "100%" }}
                 name="file"
                 label="Choose Your Flat Photo"
                 icon={<CloudUploadIcon />}
@@ -138,160 +157,13 @@ const PostAd = () => {
               />
             )}
           </Box>
-          <Button type="submit" fullWidth variant="contained">Post Ad</Button>
+          <Button type="submit" fullWidth variant="contained">
+            Post Ad
+          </Button>
         </Form>
-
-        {/* <Box component="form" noValidate onSubmit={handleSubmit} sx={{ display: "", justifyContent: "center" }}>
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="name"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="squareFeet"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="totalBedrooms"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="totalRooms"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="utilitiesDescription"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="location"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="description"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="amenities"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="rent"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="advanceAmount"
-            size="small"
-          />
-
-          <Button type="submit" fullWidth variant="contained">Post Ad</Button>
-        </Box> */}
       </Box>
     </Box>
   );
 };
 
 export default PostAd;
-
-/*  <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Box
-        sx={{
-          boxShadow:
-            "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
-          width: "350px",
-          padding: "30px",
-        }}
-      >
-        <Typography
-          sx={{ textAlign: "center", fontSize: "25px", marginBottom: "20px" }}
-        >
-          Post your Flat or Rooms
-        </Typography>
-
-      </Box>
-    </Box> */
-
-
-{/*  <Box  component="form" noValidate onSubmit={handleSubmit} sx={{ display: "", justifyContent: "center" }}>
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="name"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="squareFeet"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="totalBedrooms"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="totalRooms"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="utilitiesDescription"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="location"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="description"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="amenities"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="rent"
-            size="small"
-          />
-          <TextField
-            sx={{ width: "100%", marginBottom: "15px" }}
-            id="outlined-helperText"
-            label="advanceAmount"
-            size="small"
-          />
-
-          <Button type="submit" fullWidth variant="contained">Post Ad</Button>
-        </Box> */}
