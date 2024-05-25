cc7b296fc75503abddee8c997bffbe2494c5481d
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-05-26 00:16:32 +0600

    create user

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,157 +1,326 @@
 "use client";
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Input from '@/components/Forms/Input';
-import Form from '@/components/Forms/Form';
-import { useRouter } from 'next/navigation';
-import { FieldValues } from 'react-hook-form';
-import { toast } from 'sonner';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
-import { modifyPayload } from '@/utils/modifyPayload';
-import { registerUser } from '@/services/actions/registerUser';
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Form from "@/components/Forms/Form";
+import Input from "@/components/Forms/Input";
+import { FieldValues } from "react-hook-form";
+import { useState } from "react";
+import Link from "next/link";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerUser } from "@/services/actions/registerUser";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { toast } from "sonner";
+import { storeUserInfo } from "@/services/auth.services";
+import { useRouter } from "next/navigation";
+import { userLogin } from "@/services/actions/userLogin";
 
 export const userValidationSchema = z.object({
-    name: z.string().min(2, "Please enter your name!"),
-    profession: z.string().min(2, "Please enter your profession!"),
-    address: z.string().min(2, "Please enter your address!"),
-})
+  name: z.string().min(2, "Please enter your name!"),
+  profession: z.string().min(2, "Please enter your profession!"),
+  address: z.string().min(2, "Please enter your address!"),
+});
 
 export const registerValidationSchema = z.object({
-    username: z.string().min(2, "Please enter your username!"),
-    Email: z.string().email('Please enter a valid email!'),
-    password: z.string().min(6, "Please enter password 6 carecter"),
-    role: z.boolean(),
-    user: userValidationSchema
-})
+  username: z.string().min(2, "Please enter your username!"),
+  Email: z.string().email("Please enter a valid email!"),
+  password: z.string().min(6, "Please enter password 6 carecter"),
+  role: z.boolean(),
+  user: userValidationSchema,
+});
 
 export const defaultValues = {
-    username: "",
-    email: "",
-    password: "",
-    role: '',
-    user: {
-        name: '',
-        profession: "",
-        address: "",
-    }
+  username: "",
+  email: "",
+  password: "",
+  role: "",
+  user: {
+    name: "",
+    profession: "",
+    address: "",
+  },
 };
 
 const RegisterPage = () => {
-    //const router = useRouter();
+  const [role, setRole] = useState("");
+  const router = useRouter();
 
-    const handleRegister = async (values: FieldValues) => {
-           const data = modifyPayload(values);
-        try {
-             const res = await registerUser(data);
-            // if (res?.data.id) {
-            //   toast.success(res?.message);
+  const handleChange = (event: SelectChangeEvent) => {
+    setRole(event.target.value);
+  };
 
-            //   const result = await userLogi({
-            //     password: values.password,
-            //     email: values.patient.email,
-            //   });
+  const handleRegister = async (values: FieldValues) => {
+    values.role = role;
+    const data = modifyPayload(values);
+    try {
+      const res = await registerUser(data);
+      if (res?.data.id) {
+        toast.success(res?.message);
 
-            //   if (result?.data?.accessToken) {
-            //     storeUserInfo({ accessToken: result?.data?.accessToken });
-            //     toast.success(res?.message);
-            //     router.push("/dashboard");
-            //   }
-            // }
-        } catch (err: any) {
-            console.log(err.message);
+        const result = await userLogin({
+          password: values.password,
+          email: values.email,
+        });
+        console.log(result);
+        if (result?.data?.accessToken) {
+          storeUserInfo({ accessToken: result?.data?.accessToken });
+          // toast.success(result?.message);
+          router.push("/dashboard");
         }
-    };
+      }
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
 
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up With Flat Share
+        </Typography>
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <Box component="form" noValidate sx={{ mt: 3 }}>
-                    <Form
-                        onSubmit={handleRegister}
-                        resolver={zodResolver(registerValidationSchema)}
-                        defaultValues={defaultValues}
-                    >
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <Input
-                                    name="name"
-                                    required
-                                    fullWidth
-                                    label="First Name"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Input
-                                    required
-                                    fullWidth
-                                    label="Last Name"
-                                    name="lastName"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Input
-                                    required
-                                    fullWidth
-                                    label="Email Address"
-                                    name="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Input
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Form>
-                </Box>
-            </Box>
-        </Container>
-    );
-}
+        <Box sx={{ mt: 2 }}>
+          <Form
+            onSubmit={handleRegister}
+            // resolver={zodResolver(registerValidationSchema)}
+            defaultValues={{}}
+          >
+            <Input
+              sx={{ mb: "20px" }}
+              name="user.name"
+              required
+              fullWidth
+              label="Name"
+            />
+
+            <Input
+              sx={{ mb: "20px" }}
+              required
+              fullWidth
+              label="Username"
+              name="username"
+            />
+            <Input
+              sx={{ mb: "20px" }}
+              required
+              fullWidth
+              label="Email"
+              name="email"
+              type="email"
+            />
+            <Input
+              sx={{ mb: "20px" }}
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+            />
+            <FormControl sx={{ mb: "20px" }} fullWidth size="small">
+              <InputLabel id="demo-select-small-label1">
+                Purpose of opening account ?
+              </InputLabel>
+              <Select
+                required
+                fullWidth
+                labelId="demo-select-small-label1"
+                value={role}
+                label="Purpose of opening account ?"
+                onChange={handleChange}
+              >
+                <MenuItem value="BUYER">BUYER</MenuItem>
+                <MenuItem value="SELLER">SELLER</MenuItem>
+              </Select>
+            </FormControl>
+            <Input
+              sx={{ mb: "20px" }}
+              required
+              fullWidth
+              name="user.profession"
+              label="Profession"
+              type="string"
+            />
+            <Input
+              sx={{ mb: "20px" }}
+              required
+              fullWidth
+              name="user.address"
+              label="Address"
+              type="string"
+            />
+            <Button type="submit" fullWidth variant="contained" sx={{ mb: 2 }}>
+              Sign Up
+            </Button>
+          </Form>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <small>Already have an account? </small>
+              <small style={{ color: "purple" }}>
+                <Link href="/login">{"Sign in"}</Link>
+              </small>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default RegisterPage;
+
+/* "use client";
+
+
+const RegisterPage = () => {
+  //const router = useRouter();
+  const [role, setRole] = useState("");
+  const handleChange = (event: SelectChangeEvent) => {
+    setRole(event.target.value);
+  };
+
+  const handleRegister = async (values: FieldValues) => {
+    const data = modifyPayload(values);
+    try {
+      const res = await registerUser(data);
+      if (res?.data.id) {
+        toast.success(res?.message);
+
+        const result = await userLogi({
+          password: values.password,
+          email: values.patient.email,
+        });
+
+        if (result?.data?.accessToken) {
+          storeUserInfo({ accessToken: result?.data?.accessToken });
+          toast.success(res?.message);
+          router.push("/dashboard");
+        }
+      }
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 3 }}>
+          <Form
+            onSubmit={handleRegister}
+            resolver={zodResolver(registerValidationSchema)}
+            defaultValues={defaultValues}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Input name="name" required fullWidth label="Name" />
+              </Grid>
+              <Grid item xs={12}>
+                <Input required fullWidth label="Username" name="username" />
+              </Grid>
+              <Grid item xs={12}>
+                <Input required fullWidth label="Email Address" name="email" />
+              </Grid>
+              <Grid item xs={12}>
+                <Input
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth size="small">
+                  <InputLabel id="demo-select-small-label1">
+                    Purpose of opening account ?
+                  </InputLabel>
+                  <Select
+                    fullWidth
+                    labelId="demo-select-small-label1"
+                    value={role}
+                    label="Purpose of opening account ?"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="BUYER">BUYER</MenuItem>
+                    <MenuItem value="SELLER">SELLER</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <Input
+                  required
+                  fullWidth
+                  name="profession"
+                  label="Profession"
+                  type="string"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Input
+                  required
+                  fullWidth
+                  name="address"
+                  label="Address"
+                  type="string"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login">Already have an account? Sign in</Link>
+              </Grid>
+            </Grid>
+          </Form>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default RegisterPage;
+ */
