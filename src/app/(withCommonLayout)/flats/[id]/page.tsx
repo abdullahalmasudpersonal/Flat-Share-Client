cf78f72a30b43954ce6f3548fecff83d51dc6367
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-05-30 01:50:33 +0600

    update flat detail page

--- src/app/(withCommonLayout)/flats/[id]/page.tsx
+++ src/app/(withCommonLayout)/flats/[id]/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 import { useGetSingleFlatQuery } from "@/redux/api/flatApi";
-import { Box, CardMedia, Container, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import { formatLocalTime } from "@/components/Shared/Date&Time/Date";
+import FlatAccordian from "../components/FlatAccordion";
 
 type TParams = {
   params: {
@@ -12,10 +25,10 @@ type TParams = {
 };
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
@@ -24,40 +37,96 @@ const FlatDetailPage = ({ params }: TParams) => {
   const { data, isLoading } = useGetSingleFlatQuery(id);
   // console.log(useGetSingleFlatQuery(id));
   return (
-
-    <Box marginTop='120px' marginBottom='120px'>
+    <Box marginTop="120px" marginBottom="120px">
       <Container>
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container  spacing={{ xs: 1, sm: 2, md: 4 }} columns={16} direction={{ xs: 'column', sm: 'row' }} >
-            <Grid item xs={8}>
-            <CardMedia
-                component="img"
-                height="194"
-                image={data?.flatPhoto}
-                alt="Paella dish"
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <Item>xs=8</Item>
-            </Grid>
+          <Grid
+            container
+            spacing={{ xs: 1, sm: 2, md: 4 }}
+            columns={16}
+            direction={{ xs: "column", sm: "row" }}
+          >
+            {isLoading ? (
+              "Loading..."
+            ) : (
+              <>
+                <Grid item xs={8}>
+                  <CardMedia
+                    component="img"
+                    height="194"
+                    image={data?.flatPhoto}
+                    alt="Paella dish"
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography fontSize="21px" fontWeight="bold">
+                    {data?.flatName}
+                  </Typography>
+                  <Box display="flex" mt="8px" mx="-2px">
+                    <QueryBuilderIcon fontSize="small" sx={{ color: "gray" }} />
+                    <Typography fontWeight="500">
+                      &nbsp;{formatLocalTime(data?.createdAt)}
+                    </Typography>
+                  </Box>
+                  <Box display="flex" mt="8px" mx="-5px">
+                    <LocationOnIcon sx={{ color: "gray" }} />
+                    <Typography fontWeight="500">{data?.location}</Typography>
+                  </Box>
+                  <Typography display="flex" mt="8px">
+                    <Typography fontWeight="500">Available:</Typography>
+                    &nbsp;
+                    {data?.availability ? (
+                      <>
+                        {"Yes"}&nbsp;
+                        <CheckCircleIcon
+                          fontSize="small"
+                          sx={{ color: "green" }}
+                        />
+                      </>
+                    ) : (
+                      <>
+                        {"No"}&nbsp;
+                        <DoNotDisturbIcon
+                          fontSize="small"
+                          sx={{ color: "red" }}
+                        />
+                      </>
+                    )}
+                    &nbsp;
+                  </Typography>
+                  <Typography display="flex" mt="8px">
+                    <Typography fontWeight="500">Flat Size:</Typography>
+                    &nbsp;{data?.squareFeet}&nbsp;Square Feet
+                  </Typography>
+                  <Typography display="flex" mt="8px">
+                    <Typography fontWeight="500">Bed Rooms:</Typography>
+                    &nbsp;{data?.totalBedrooms}
+                  </Typography>
+                  <Typography display="flex" mt="8px">
+                    <Typography fontWeight="500">Total Rooms:</Typography>
+                    &nbsp;{data?.totalRooms}
+                  </Typography>
+                  <Typography display="flex" mt="8px">
+                    <Typography fontWeight="500">Rent:</Typography>
+                    &nbsp;{data?.rent}&nbsp;Tk
+                  </Typography>
+                  <Typography display="flex" mt="8px">
+                    <Typography fontWeight="500">Advance Amount:</Typography>
+                    &nbsp;{data?.advanceAmount}&nbsp;Tk
+                  </Typography>
+
+                  <Box mt="20px">
+                    <Button variant="contained">Booking Request</Button>
+                  </Box>
+                  <Box mt="20px">
+                    <FlatAccordian data={data} />
+                  </Box>
+                </Grid>
+              </>
+            )}
           </Grid>
         </Box>
       </Container>
-      {/*   <Container >
-        <Box>
-          <Grid>
-            <Grid >
-              <CardMedia
-                component="img"
-                height="194"
-                image={data?.flatPhoto}
-                alt="Paella dish"
-              />
-            </Grid>
-            <Grid></Grid>
-          </Grid>
-        </Box>
-      </Container> */}
     </Box>
   );
 };
