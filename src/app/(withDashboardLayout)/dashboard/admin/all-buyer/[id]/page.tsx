9573ce03e1304c4af4d39906130743d6d0302d1c
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-06-02 01:45:51 +0600

    another update

--- src/app/(withDashboardLayout)/dashboard/admin/all-buyer/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/all-buyer/[id]/page.tsx
@@ -1,23 +1,136 @@
-"use client"
-import { useGetSingleBuyerQuery } from '@/redux/api/userApi';
-import { Box, Button, Container, Grid } from '@mui/material';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import { useGetSingleBuyerQuery } from "@/redux/api/userApi";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Modal,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import UserProfileInfo from "../components/UserProfileInfo";
+import profileAltLogo from "@/assets/profile/person-icon.png";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 type TParams = {
-    params: {
-        id: string;
-    };
+  params: {
+    id: string;
+  };
 };
-const EditBuyerPage = ({ params }: TParams) => {
-    const id = params?.id;
-    const { data, isLoading } = useGetSingleBuyerQuery(id)
-    return (
-        <>
-            <Container sx={{ mt: 4 }}>
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  // border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const BuyerDetailPage = ({ params }: TParams) => {
+  const id = params?.id;
+  const { data, isLoading } = useGetSingleBuyerQuery(id);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [role, setRole] = React.useState<string | undefined>(undefined);
+  const handleChange = (event: SelectChangeEvent) => {
+    setRole(event.target.value);
+  };
+
+  type Inputs = {
+    status: string;
+    role: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            mb="10px"
+          >
+            Update User Status & Role
+          </Typography>
+          <Box display="flex">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Box>
+                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                  <InputLabel id="demo-select-small-label">Role</InputLabel>
+                  <Select
+                    id="role"
+                    {...register("role")}
+                    labelId="demo-select-small-label"
+                    value={role}
+                    label="Role"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="BUYER">BUYER</MenuItem>
+                    <MenuItem value="SELLER">SELLER</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                  <InputLabel id="demo-select-small-label">Status</InputLabel>
+                  <Select
+                    id="status"
+                    {...register("status")}
+                    labelId="demo-select-small-label"
+                    value={role}
+                    label="Status"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="BLOCKED">BLOCKED</MenuItem>
+                    <MenuItem value="ACTIVE">ACTIVE</MenuItem>
+                    <MenuItem value="PENDING">PENDING</MenuItem>
+                    <MenuItem value="DELETED">DELETED</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <Button type="submit" variant="contained">
+                Update
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Modal>
+
+      <Container sx={{ mt: 4 }}>
         <Grid container spacing={4}>
-          <Grid xs={12} md={4}>
+          <Grid xs={12} md={4} padding="16px">
             <Box
+              my={3}
               sx={{
                 height: 300,
                 width: "100%",
@@ -27,64 +140,38 @@ const EditBuyerPage = ({ params }: TParams) => {
                   "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
               }}
             >
-              {data?.profilePhoto ? (
+              {data?.userProfile?.profilePhoto ? (
                 <Image
                   height={300}
                   width={400}
-                  src={data?.profilePhoto}
+                  src={data?.userProfile?.profilePhoto}
                   alt="User Photo"
                 />
               ) : (
                 <Image
                   height={300}
                   width={400}
-                  src={data?.profilePhoto}
+                  src={profileAltLogo}
                   alt="User Photo"
                 />
               )}
             </Box>
-            <Box my={3} display="flex" justifyContent="center">
-            {/*   {updating ? (
-                <p>Uploading...</p>
-              ) : (
-                <AutoFileUploader
-                  name="file"
-                  label="Choose Your Profile Photo"
-                  icon={<CloudUploadIcon />}
-                  onFileUpload={fileUploadHandler}
-                  variant="outlined"
-                />
-              )} */}
-            </Box>
-
             <Button
               variant="contained"
               fullWidth
-             /*  endIcon={<ModeEditIcon />}
-              onClick={() => setIsModalOpen(true)} */
+              endIcon={<ModeEditIcon />}
+              onClick={handleOpen}
             >
-              Edit Profile
+              Updatge
             </Button>
-            <Box my={3} >
-              <Link href="/dashboard/change-password">
-                <Button
-                  variant="contained"
-                  fullWidth
-                 /*  endIcon={<LockResetIcon />}
-                  onClick={() => setIsModalOpen(true)} */
-                >
-                  Change password
-                </Button>
-              </Link>
-            </Box>
           </Grid>
-          <Grid xs={12} md={8} border='1px solid gray'>
-            {/* <UserProfileInfo data={data} /> */}
+          <Grid xs={12} md={8} padding="16px">
+            <UserProfileInfo data={data} />
           </Grid>
         </Grid>
       </Container>
-        </>
-    );
+    </>
+  );
 };
 
-export default EditBuyerPage;
+export default BuyerDetailPage;
--- src/app/(withDashboardLayout)/dashboard/admin/all-buyer/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/all-buyer/page.tsx
@@ -1,89 +1,69 @@
-'use client'
+"use client";
 import { useGetAllBuyerQuery } from "@/redux/api/userApi";
 import {
   Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
   Paper,
-  Select,
   SelectChangeEvent,
-  Table, TableBody,
-  TableCell, TableContainer, TableHead, TableRow
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
 } from "@mui/material";
 import { useState } from "react";
-import BorderColorIcon from '@mui/icons-material/BorderColor';
 import Link from "next/link";
 
-
-
-
 const AllBuyerPage = () => {
-  const [age, setAge] = useState('');
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
-  };
-
   const { data: buyerData, isLoading } = useGetAllBuyerQuery({});
-  // console.log("data",data);
+
   return (
     <>
-      {
-        isLoading ? "Loading..." :
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell align="right">Email</TableCell>
-                  <TableCell align="right">Role</TableCell>
-                  <TableCell align="right">Status</TableCell>
-                  <TableCell align="right">Profession</TableCell>
-                  <TableCell align="right">Address</TableCell>
-                  <TableCell align="center">Details</TableCell>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Role</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Profession</TableCell>
+                <TableCell align="right">Address</TableCell>
+                <TableCell align="center">Details</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {buyerData?.map((data: any) => (
+                <TableRow
+                  key={data?.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {data?.userProfile?.name}
+                  </TableCell>
+                  <TableCell align="right">{data?.email}</TableCell>
+                  <TableCell align="right">{data?.role}</TableCell>
+                  <TableCell align="right">{data?.status}</TableCell>
+                  <TableCell align="right">
+                    {data?.userProfile?.profession}
+                  </TableCell>
+                  <TableCell align="right">
+                    {data?.userProfile?.address}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Link href={`/dashboard/admin/all-buyer/${data?.id}`}>
+                      <Button variant="contained">Details</Button>
+                    </Link>
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {buyerData?.map((data: any) => (
-                  <TableRow
-                    key={data?.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {data?.userProfile?.name}
-                    </TableCell>
-                    <TableCell align="right">{data?.email}</TableCell>
-                    <TableCell align="right">{data?.role}</TableCell>
-                    <TableCell align="right">{data?.status}</TableCell>
-                    <TableCell align="right">{data?.userProfile?.profession}</TableCell>
-                    <TableCell align="right">{data?.userProfile?.address}</TableCell>
-                    <TableCell align="center"><Link href={`/dashboard/admin/all-buyer/${data?.id}`}><Button variant="contained">Details</Button></Link></TableCell>
-                   {/*  <TableCell align="right" sx={{paddingRight:'10px'}}>
-                      <FormControl sx={{ m: 1, minWidth: 120 }} size="small" >
-                        <InputLabel id="demo-select-small-label">Age</InputLabel>
-                        <Select
-                          labelId="demo-select-small-label"
-                          id="demo-select-small"
-                          value={age}
-                          label="Age"
-                          onChange={handleChange}
-                        >
-                          <MenuItem value="">
-                            <em>None</em>
-                          </MenuItem>
-                          <MenuItem value={10}>Ten</MenuItem>
-                          <MenuItem value={20}>Twenty</MenuItem>
-                          <MenuItem value={30}>Thirty</MenuItem>
-                        </Select>
-                      </FormControl>
-                    </TableCell> */}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-      }
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </>
   );
 };
