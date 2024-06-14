06de11ea1f22677ff86ad41b033c1d358cad9314
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-06-15 01:04:47 +0600

    anoter update

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,13 +1,16 @@
 "use client";
+import { useEffect } from "react";
 import DashboardDrawer from "../../components/Dashboard/DashboardDrawer/DashboardDrawer";
 import { isLogedIn } from "../../services/auth.services";
 import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
-  if (!isLogedIn()) {
-    return router.push("/login");
-  }
+  useEffect(() => {
+    if (!isLogedIn()) {
+      router.push("/login");
+    }
+  }, [router]);
 
   return <DashboardDrawer>{children}</DashboardDrawer>;
 };
