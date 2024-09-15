76cbf3891f9af51577908d0bc5a8c6d3c4ca400b
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-09-16 02:18:27 +0600

    another update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -7,8 +7,8 @@ import { axiosBaseQuery } from "../../helpers/axios/axiosBaseQuery";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-     baseUrl: "https://assignment-09-server.vercel.app/api/v1", 
-   // baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://assignment-09-server.vercel.app/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
